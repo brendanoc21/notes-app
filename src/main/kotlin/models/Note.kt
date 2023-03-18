a5d6609ab5d6e8dfb56fd38c3ef9d1b74196301f
Brendan O'Connor <brendanoconnor2030@gmail.com>
2023-03-18 21:39:01 +0000

    Override Note toString to make it look neater

--- src/main/kotlin/models/Note.kt
+++ src/main/kotlin/models/Note.kt
@@ -1,4 +1,7 @@
 package models
 
 data class Note(var noteTitle: String, var notePriority: Int, var noteCategory: String, var isNoteArchived :Boolean){
+    override fun toString(): String = """
+    Title: $noteTitle, Priority: $notePriority, Category: $noteCategory, Archived: $isNoteArchived
+    """.trimIndent()
 }
